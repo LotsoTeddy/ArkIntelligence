edd33652ebbb3be462b810930595f29c222904c0
root <fyz@mail.nankai.edu.cn>
2025-04-01 19:54:28 +0800

    add utils init

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,6 +1,6 @@
 from arkintelligence.model import ArkModel
 
-# ======== 1. Model usage ========
-model = ArkModel(model="doubao-1.5-pro-32k-250115", enbale_context=True)
+model = ArkModel(model="doubao-1.5-pro-32k-250115")
+
 response = model.chat(prompt="Who are you?")
-print(response)
+response
