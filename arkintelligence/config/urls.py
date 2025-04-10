bc34f8625a963596f113f594d84f8a5b1da3355a
root <fyz@mail.nankai.edu.cn>
2025-04-10 19:05:01 +0800

    update urls

--- arkintelligence/config/urls.py
+++ arkintelligence/config/urls.py
@@ -1,7 +1,5 @@
 CONTEXT_API_BASE_URL = "https://ark.cn-beijing.volces.com/api/v3/context/create"
 
-TEXT_API_BASE_URL = (
-    "https://ark.cn-beijing.volces.com/api/v3/chat/completions"  # For text models
-)
+TEXT_API_BASE_URL = "https://ark.cn-beijing.volces.com/api/v3/"  # For text models
 
 VISION_API_BASE_URL = "https://ark.cn-beijing.volces.com/api/v3/contents/generations/tasks"  # For vision models
